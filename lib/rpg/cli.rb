a4764293c624e4bf621b1f5d15644d0547613496
4anth0ny1 <acatullo4@gmail.com>
2021-02-03 19:16:50 -0600

    added hero class and cli methods

--- lib/rpg/cli.rb
+++ lib/rpg/cli.rb
@@ -1,9 +1,60 @@
-## This class will allow the hero and the enemy to do battle 
-
 class Cli 
 
-def call 
-    puts "yo yo"
-end
+    ## what i could try is the make each player class a sub class of hero and make a hash for each player class and then use mass assignment to allow me to add attributes to the player class. 
+
+    def call 
+        greeting
+        choose_your_player
+        player_selection
+        muster
+    end
+
+    def greeting 
+        puts ""
+        puts "Welcome to RPG Cli"
+        puts ""
+    end 
+
+    def choose_your_player 
+        puts "Choose your character by inputting a number ..."
+        puts ""
+        print "1. Viking: "
+        puts "Weap: Axe & Shield, Arm: 50, Str: 100, Dex: 50, Mag: 0"  
+        puts "" 
+        print "2. Druid: "
+        puts "Weap: Staff, Arm: 0, Str: 0, Dex: 25, Mag: 100"   
+        puts ""      
+    end 
+
+    def player_selection 
+        input = gets.strip.downcase 
+        if input == "1"
+            puts ""      
+            puts "You choose to be a viking."
+            puts ""      
+            puts "I'm Ragnar. With my skill in battle we shall conquer all!"
+            puts ""      
+        elsif input == "2"
+            puts ""      
+            puts "You chose to be a druid."
+            puts ""      
+            puts "I'm Merlin. We must defend this world."
+            puts ""      
+        else
+            puts "Invalid Entry. Try Again."
+        end 
+    end 
+
+    def muster(input)
+        puts ""
+        puts "Let's prepare for battle."
+        puts "You have upgrade points to spend."
+        puts "Which attribute would you like to upgrade?"
+        puts ""
+        if input == "1" 
+            show_hero
+        end     
+
+    end 
 
 end 
--- lib/rpg/hero.rb
+++ lib/rpg/hero.rb
@@ -1,5 +1,49 @@
 class Hero 
 
-    attr_accessor :name, :species, :strength, :dexterity
+    attr_accessor :race, :strength, :dexterity, :magic, :weapon, :armor, :life
+
+    @@all = []
+
+    def initalize(race, strength, dexterity, magic, weapon, armor, life = 100) 
+        @race = race
+        @strength = strength
+        @dexterity = dexterity
+        @magic = magic
+        @weapon = weapon
+        @armor = armor 
+        @life = life
+        save
+    end 
+
+    def show_hero(race, strength, dexterity, magic, weapon, armor, life = 100) 
+        print "Race: "
+        puts race 
+        print "Strength: "
+        puts strength
+        print "Dexterity: "
+        puts dexterity
+        print "Magic: "
+        puts magic
+        print "Weapon: "
+        puts weapon
+        print "Armor: "
+        puts armor
+        print "Life: "
+        puts life
+    end 
+
+    
+
+    def save 
+        @@all << self
+    end 
+
+    def self.all 
+        @@all 
+    end 
 
 end
+
+viking = Hero.new
+puts viking.show_hero("Viking", 100, 50, 0, "Axe and Shield", 50, 100)
+
